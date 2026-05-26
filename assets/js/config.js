ec78569dcfdc5a7f5bb75c557c9e3ee5209ca498
Profe. Santiago <107638111+profesantiagomtz@users.noreply.github.com>
2026-05-25 23:54:14 -0500

    Update config.js

--- assets/js/config.js
+++ assets/js/config.js
@@ -1,4 +1,4 @@
 // Configuración de Supabase.
 // Pega aquí tu anon public key completa.
 const SUPABASE_URL = 'https://mllxxtibwzijewfclnuj.supabase.co';
-const SUPABASE_ANON_KEY = 'PEGA_AQUI_TU_ANON_KEY';
+const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1sbHh4dGlid3ppamV3ZmNsbnVqIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzk3MzUyMDMsImV4cCI6MjA5NTMxMTIwM30.SA8QP-vmHBAIimRkP8mFbZ5EysepqUd7cPV7aShZfwI';
