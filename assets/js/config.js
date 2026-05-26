e691863fb1c55f10c3ac1acdda793c4b1facdfbd
Profe. Santiago <107638111+profesantiagomtz@users.noreply.github.com>
2026-05-25 23:04:05 -0500

    Create config.js

---
+++ assets/js/config.js
@@ -0,0 +1,5 @@
+const SUPABASE_URL =
+'https://mllxxtibwzijewfclnuj.supabase.co';
+
+const SUPABASE_ANON_KEY =
+'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1sbHh4dGlid3ppamV3ZmNsbnVqIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzk3MzUyMDMsImV4cCI6MjA5NTMxMTIwM30.SA8QP-vmHBAIimRkP8mFbZ5EysepqUd7cPV7aShZfwI';
